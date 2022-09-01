7810754da36e426e6eaf40a123a00e4dd05dbc28
Gianluca De Cola <gianluca.decola@prima.it>
2022-09-01 09:37:46 +0200

    test input on databricks.

--- foo/foo.py
+++ foo/foo.py
@@ -1,2 +1,6 @@
 def test():
-    print("Hello")
+    out = input("enter Y or N")
+    if out == "Y":
+        print("You entered Y")
+    else:
+        print("You entered N or another.")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='foo',
-    version='1.1',
+    version='1.2',
     py_modules=['foo'],
     packages=find_packages()
 )
